efd14ff5a566762cf28b19f07791b025dfac11d6
ramteketrupti2604-creator <ramteketrupti2604@gmail.com>
2026-01-13 11:14:04 +0530

    Add files via upload

---
+++ passorfail.c
@@ -0,0 +1,12 @@
+//write a program to check mark obtained by person pass or not
+#include<stdio.h>
+int main(){
+    int mark=40;
+    if(mark>50){
+        printf("HE IS PASS");
+    }
+    else{
+        printf("HE IS NOT PASS");
+    }
+    return 0;
+}
